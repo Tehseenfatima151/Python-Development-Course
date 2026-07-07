86fcdcdf34fc3a79a0c5593faa876a0788140403
Tehseenfatima151 <shinefatim151@gmail.com>
2026-07-07 14:56:33 +0500

    Day1
    simple printing, user input, concatenation concepts

---
+++ Day 1/project1.py
@@ -0,0 +1,22 @@
+#task 1
+print("Hello Tehseen")
+age=input("Enter Your age:\n")
+print("You are " + age + " years old.")
+city=input("Enter Your city:\n")
+print("You live in " + city + ".")
+
+#task2
+name=input("Enter your name:\n")
+print("Hi, my name is " + name + "!")
+degree=input("Enter Your degree:\n")
+uni=input("Enter Your university:\n")
+print("I study " + degree + " at " + uni + ".")
+print("Nice to meet you!")
+
+#task3
+first_name=input("Enter Your First Name:\n")
+last_name=input("Enter Your Last Name:\n")
+print("Welcome, " + first_name + " " + last_name + "!")
+age=input("Enter Your age:\n")
+print("You are " + age + " years old.")
+print("Have a great day!")
