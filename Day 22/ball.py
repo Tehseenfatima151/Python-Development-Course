e189933cff3ede1872d9d4df95314881283a7cc6
Tehseenfatima151 <shinefatim151@gmail.com>
2026-07-31 13:45:43 +0500

    pong part 1

---
+++ Day 22/ball.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+
+
+class Ball(Turtle):
+    """Models the ball that moves around and bounces off walls."""
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+        self.x_move = 10
+        self.y_move = 10
+        self.move_speed = 0.1
+
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
+
+    def bounce_y(self):
+        """Reverses vertical direction (used when ball hits top/bottom wall)."""
+        self.y_move *= -1
